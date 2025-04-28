89508a34bdb5a993504d0d728432e576b3088b61
10DavidFranco <david.franco02@utrgv.edu>
2025-04-28 11:32:50 -0500

    Employee and Task models migrated

--- tasktrek/models.py
+++ tasktrek/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 64)
+    is_admin = models.BooleanField(default=False)
+    tasks = models.CharField(max_length = 64)
+
+class Task(models.Model):
+    id = models.AutoField(primary_key = True)
+    description = models.CharField(max_length = 100)
+    due_date = models.DateField()
+    status = models.BooleanField()
+
--- tasktrek/views.py
+++ tasktrek/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-
+from .models import Employee, Task
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
 def login(request):
-    return render(request, "login.html")
+    employees = Employee.objects.all()
+    return render(request, "login.html", {'employees': employees})
 
+#The user route perhaps needs to take in an employee object of that specific user
+#Need to find a way to pass in the employee information from the login screen.
 def user(request):
-    return render(request, "user.html")
+    tasks = Task.objects.all()
+    return render(request, "user.html", {'tasks': tasks})
 
 def superuser(request):
-    return render(request, "superuser.html")
+    employees = Employee.objects.all()
+    tasks = Task.objects.all()
+    return render(request, "superuser.html", {'employees': employees, 'tasks': tasks})
