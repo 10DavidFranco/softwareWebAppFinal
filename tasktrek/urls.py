6866494b970b44a48d1aec87ddc8e3508c324a90
SabinoRodea <sabino.rodea01@utrgv.edu>
2025-05-10 22:53:34 -0500

    changes

--- tasktrek/urls.py
+++ tasktrek/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.login, name="login"),
+    path('signup/',views.signup_view, name='singup'),
     path('user/<int:employee_id>/', views.user, name="user"),
     path('superuser/<int:employee_id>/', views.superuser, name="superuser"),
     path('handlelogin/', views.handlelogin, name= "handlelogin"),
--- tasktrek/views.py
+++ tasktrek/views.py
@@ -13,6 +13,8 @@ def login(request):
     employees = Employee.objects.all()
     return render(request, "login.html", {'employees': employees})
 
+def signup_view(request):
+    return render(request, 'signup.html')
 #The user route perhaps needs to take in an employee object of that specific user
 #Need to find a way to pass in the employee information from the login screen.
 def user(request, employee_id):
